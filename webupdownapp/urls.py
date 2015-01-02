16e10188f3099b302540b0425bf7dc79bbabcbac
quantumhair <jamoody@gmail.com>
2015-01-02 10:10:25 -0500

    first commit

---
+++ webupdownapp/urls.py
@@ -0,0 +1,30 @@
+from django.conf.urls import patterns, include, url
+
+from marketing.views import HomePage
+
+urlpatterns = patterns('',
+
+    # Marketing pages
+    url(r'^$', HomePage.as_view(), name="home"),
+
+    # Subscriber related URLs
+    url(r'^signup/$',
+        'crmapp.subscribers.views.subscriber_new', name='sub_new'
+    ),
+
+
+    # Admin URL
+
+
+    # Login/Logout URLs
+
+
+    # Account related URLs
+
+
+    # Contact related URLS
+
+
+    # Communication related URLs
+
+)
