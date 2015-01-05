3e98d029842287434375f7cb42f4336af2e967ab
quantumhair <jamoody@gmail.com>
2015-01-05 18:04:50 -0500

    create login and logout URLs

--- webupdownapp/urls.py
+++ webupdownapp/urls.py
@@ -21,7 +21,12 @@ urlpatterns = patterns('',
 	
 
     # Login/Logout URLs
-
+    (r'^login/$',
+    	'django.contrib.auth.views.login', {'template_name': 'login.html'}
+    ),
+    (r'^logout/$',
+    	'django.contrib.auth.views.logout', {'next_page': '/login/'}
+    ),
 
     # Account related URLs
 
