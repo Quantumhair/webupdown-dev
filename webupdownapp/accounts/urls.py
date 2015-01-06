c7e41e381e1c80385cf8e70680cdca2e2661700a
quantumhair <jamoody@gmail.com>
2015-01-06 10:59:01 -0500

    added edit account URL

--- webupdownapp/accounts/urls.py
+++ webupdownapp/accounts/urls.py
@@ -5,4 +5,7 @@ account_urls = patterns('',
     url(r'^$',
         'webupdownapp.accounts.views.account_detail', name='account_detail'
     ),
+        url(r'^edit/$',
+        'webupdownapp.accounts.views.account_cru', name='account_update'
+    ),
 )
