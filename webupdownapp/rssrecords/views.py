88b93af28901d995a0a346cbe308b18a24a41085
quantumhair <jamoody@gmail.com>
2015-01-07 13:34:49 -0500

    created rssrecords list view

--- webupdownapp/rssrecords/views.py
+++ webupdownapp/rssrecords/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+from .models import Rssrecord
+
+class RssRecordList(ListView):
+    model = Rssrecord
+    template_name = 'rssrecords/rssrecords_list.html'
+    context_object_name = 'rssrecords'
+
+    def get_queryset(self):
+        account_list = Rssrecord.objects.filter(owner=self.request.user)
+        return account_list
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(RssRecordList, self).dispatch(*args, **kwargs)
